f16f2ede83a9ab62126b61bde8d74469ee0db895
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-12-06 13:57:16 +0100

    Current

--- CentralWidget.py
+++ CentralWidget.py
@@ -3,12 +3,18 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget
 
 # Package: PyQt6-Charts
+# https://github.com/chey00/qchart
 class CentralWidget(QChartView):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        series = QSplineSeries()
+        series2 = QLineSeries()
+
         chart = QChart()
         chart.setTitle("Die wunderbare Welt der Mathematik")
+        chart.addSeries(series)
+        chart.addSeries(series2)
 
         axis_x = QValueAxis()
         axis_x.setRange(1, 4)
@@ -16,12 +22,20 @@ class CentralWidget(QChartView):
         axis_y = QValueAxis()
         axis_y.setRange(0, 10)
 
-        chart.addAxis(axis_x, Qt.AlignmentFlag.AlignBottom)
-        chart.addAxis(axis_y, Qt.AlignmentFlag.AlignRight)
+        chart.addAxis(axis_x, Qt.AlignmentFlag.AlignTop)
+        series.attachAxis(axis_x)
+        series2.attachAxis(axis_x)
 
-        series = QSplineSeries()
+        chart.addAxis(axis_y, Qt.AlignmentFlag.AlignLeft)
+        series.attachAxis(axis_y)
+        series2.attachAxis(axis_y)
+
+        series2.attachAxis(axis_x)
+        series2.attachAxis(axis_y)
+
         series.attachAxis(axis_x)
         series.attachAxis(axis_y)
+
         series.setName("Parabel")
         series.append(0, 0)
         series.append(1, 1)
@@ -30,9 +44,6 @@ class CentralWidget(QChartView):
         series.append(4, 16)
         series.append(5, 25)
 
-        series2 = QLineSeries()
-        series2.attachAxis(axis_x)
-        series2.attachAxis(axis_y)
         series2.setName("Gerade")
         series2.append(0, -5)
         series2.append(1, -2.5)
@@ -40,8 +51,5 @@ class CentralWidget(QChartView):
         series2.append(3, 2.5)
         series2.append(4, 5)
         series2.append(5, 7.5)
-        chart.addSeries(series)
-        chart.addSeries(series2)
 
         self.setChart(chart)
-
